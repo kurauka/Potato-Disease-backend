6aaf5c5de477274a0b20d05a5ac3adf239c5d0dd
Mohamed Abdalla Mohamed <58578637+kurauka@users.noreply.github.com>
2024-09-14 23:53:11 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -47,4 +47,4 @@ async def predict(file: UploadFile = File(...)):
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5500)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
