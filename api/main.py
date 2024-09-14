7bf37eb804c49be554b0b7dbe97d80145d76e9d5
Mohamed Abdalla Mohamed <58578637+kurauka@users.noreply.github.com>
2024-09-14 19:21:03 +0300

    Add files via upload

---
+++ api/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+app = FastAPI()
+
+# Allow CORS for all origins (adjust for production)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust to your frontend origin in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+MODEL = tf.keras.models.load_model("../models/1.keras")
+CLASS_NAMES = ["HDPE (High-Density Polyethylene)", "OTHERS", "PET (polyethylene terephthalate)", "PP (polypropylene)", "PVC (Polyvinyl chloride)"]
+
+@app.get("/ping")
+async def ping():
+    return {"message": "Hello, Badi"}
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+
+    prediction = MODEL.predict(img_batch)
+    
+    predicted_class = CLASS_NAMES[np.argmax(prediction[0])]
+    confidence = np.max(prediction[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=5500)
